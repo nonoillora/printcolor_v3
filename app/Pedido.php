ecac14d1356c099198e219e2264beee89558c6bc
Antonio Extremera <nonoillora@gmail.com>
2020-05-08 20:45:23 +0200

    [FIX] add scope and properties

--- app/Pedido.php
+++ app/Pedido.php
@@ -8,7 +8,13 @@ class Pedido extends Model
 {
     protected $primaryKey = 'idPedido';
 
-    protected $fillable = [
-        'idPedido','idCliente','idLineas','idTipoPago','totalPedido', 'isSent', 'isPaid','num_seguimiento','company_shipping','numIdentificacionPedido','totalIVA','withoutIVA'];
+    protected $table = 'pedidos';
 
+    protected $fillable = [
+        'idPedido', 'idCliente', 'idLineas', 'idTipoPago', 'totalPedido', 'isSent', 'isPaid', 'num_seguimiento', 'company_shipping', 'numIdentificacionPedido', 'totalIVA', 'withoutIVA'];
+
+    public function tipoPago()
+    {
+        return $this->hasMany(\App\TipoPago::class, 'id','idTipoPago');
+    }
 }
--- app/TipoPago.php
+++ app/TipoPago.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Pedido;
 
 class TipoPago extends Model
 {
-    //
+    protected $primaryKey = 'id';
+
+    protected $table = 'tipo_pagos';
+
+    protected $fillable = [
+        'id','nombre'
+    ];
 }
