fa11203b9e1ffb1753b03fa4b7a7213384742231
Antonio Extremera <nonoillora@gmail.com>
2020-05-16 18:38:11 +0200

    [FIX] add info about payment selected

--- resources/views/pedidos/orderSuccessRegistered.blade.php
+++ resources/views/pedidos/orderSuccessRegistered.blade.php
@@ -1,7 +1,7 @@
 @extends('template')
 @section('title')
     {{$title}}
-    @endsection
+@endsection
 @section('content')
     <div class="breadcrumbs">
         <ol class="breadcrumb">
@@ -20,8 +20,20 @@
         <br/>
     </div>
     <div class="col-lg-8 col-lg-offset-2">
-        <p>Compruebe su correo.</p>
-
+        <p>Muchas gracias por su compra y confianza depositada en nosotros.</p>
+        @if($pago==1)
+            <p>En breve, recibir&aacute;s un correo electr&oacute;nico de confirmaci&oacute;n del pedido, en el cual se explican las instrucciones para realizar la
+            transferencia y el n&uacute;mero de identificaci&oacute;n del pedido realizado. Una vez confirmada
+            la transferencia comenzaremos a preparar el pedido.</p>
+            <p>Se te notificara cuando sea enviado a la agencia de
+            transporte junto con el n&uacute;mero de seguimiento del mismo para poder seguir el
+            envi&oacute; del pedido.</p>
+        @endif
+        @if($pago==2)
+            <p>En breve, recibir&aacute;s un correo electr&oacute;nico de confirmaci&oacute;n del pedido, en el cual hay un resumen del pedido realizado.</p>
+            <p>Se te notificara cuando sea enviado a la agencia de transporte junto con el n&uacute;mero de seguimiento del
+            mismo para poder seguir el envi&oacute; del pedido.<p>
+        @endif
         <br/><br/>
     </div>
 
