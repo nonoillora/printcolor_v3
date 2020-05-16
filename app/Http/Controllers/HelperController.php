5b0179704ac0785a5c8da18f81f2cff8b4eaf02c
Antonio Extremera <nonoillora@gmail.com>
2020-05-16 20:56:47 +0200

    [FIX] update controller to get bill on pdf

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Pedido;
 use Illuminate\Http\Request;
 use Helper;
 
@@ -30,7 +31,7 @@ class HelperController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -40,13 +41,28 @@ class HelperController extends Controller
 
     public function getBill($id)
     {
-        return Helper::doBillPDF('ver',$id);
+        $pedido = Pedido::where('idPedido',$id)->first();
+        if(empty($pedido)){
+            return 'No existe este pedido';
+        }
+        $factura = Pedido::where('idPedido', $id)->first()->factura();
+        if (empty($factura)) {
+            FacturaController::createNewFactura($pedido);
+
+            Helper::saveBillPDF($id);
+        }
+        $month = date_format(date_create($pedido->created_at),'n');
+        $year =  date_format(date_create($pedido->created_at),'Y');
+        //Check if directory is created..
+        Helper::checkExistsThisFolder($year . '/' . $month . '/');
+        return Helper::doBillPDF('ver', $id);
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -57,7 +73,7 @@ class HelperController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -68,8 +84,8 @@ class HelperController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -80,7 +96,7 @@ class HelperController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
