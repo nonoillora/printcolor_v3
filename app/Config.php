f022886f0c84f9a1570d8017f4bc9f635973de15
Antonio <nonoillora@users.noreply.github.com>
2020-12-12 20:53:50 +0100

    [FIX] update composer and Config

--- app/Config.php
+++ app/Config.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Config extends Model
 {
+    protected $table = 'configs';
+
     protected $primaryKey = 'idConfig';
+
+    protected $fillable = [
+        'config_key', 'type', 'value', 'description', 'config_is_active'
+    ];
 }
